96d9b3b482d75f381587fef3c0de7201edac4dec
alexandr motorin <blackdizel@gmail.com>
2016-05-16 21:01:53 +0300

    implement shopping list adapter

--- app/src/main/java/org/byters/bcshoppinglist/ui/adapters/AdapterShoppingList.java
+++ app/src/main/java/org/byters/bcshoppinglist/ui/adapters/AdapterShoppingList.java
@@ -2,15 +2,23 @@ package org.byters.bcshoppinglist.ui.adapters;
 
 
 import android.support.annotation.LayoutRes;
+import android.support.annotation.Nullable;
 import android.support.v7.widget.RecyclerView;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.TextView;
 
 import org.byters.bcshoppinglist.R;
+import org.byters.bcshoppinglist.controllers.ControllerShoppingList;
+import org.byters.bcshoppinglist.model.ShoppingListItem;
 
 
 public class AdapterShoppingList extends RecyclerView.Adapter<AdapterShoppingList.ViewHolder> {
+
     public AdapterShoppingList() {
     }
 
@@ -32,18 +40,41 @@ public class AdapterShoppingList extends RecyclerView.Adapter<AdapterShoppingLis
 
     @Override
     public int getItemCount() {
-        return 0;
+        return ControllerShoppingList.getInstance().getCount();
+    }
+
+    public void addItem() {
+        //todo: replace with notifyItemAdded
+        notifyDataSetChanged();
     }
 
 
-    public class ViewHolder extends RecyclerView.ViewHolder {
+    public class ViewHolder extends RecyclerView.ViewHolder
+            implements CheckBox.OnCheckedChangeListener {
+        private TextView tvTitle;
+        private CheckBox cbItem;
+
+        @Nullable
+        private ShoppingListItem item;
+
         public ViewHolder(View itemView) {
             super(itemView);
+            tvTitle = (TextView) itemView.findViewById(R.id.tvTitle);
+            cbItem = (CheckBox) itemView.findViewById(R.id.cbItem);
+
+            cbItem.setOnCheckedChangeListener(this);
         }
 
         public void setData(int position) {
-
+            item = ControllerShoppingList.getInstance().getItem(position);
+            String text = ControllerShoppingList.getInstance().getTitle(position);
+            tvTitle.setText(TextUtils.isEmpty(text) ? "" : text);
+            cbItem.setChecked(ControllerShoppingList.getInstance().isChecked(position));
         }
 
+        @Override
+        public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+            ControllerShoppingList.getInstance().setItemChecked(buttonView.getContext(), item, isChecked);
+        }
     }
 }
