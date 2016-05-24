d735d35ad7ace2fe15adb2e92dc6ce7230f918d7
alexandr motorin <blackdizel@gmail.com>
2016-05-24 23:19:52 +0300

    add market map activity

--- app/src/main/java/org/byters/bcshoppinglist/ui/activity/ActivityMarketMap.java
+++ app/src/main/java/org/byters/bcshoppinglist/ui/activity/ActivityMarketMap.java
@@ -2,11 +2,64 @@ package org.byters.bcshoppinglist.ui.activity;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Point;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.text.TextUtils;
 
-public class ActivityMarketMap extends ActivityBase {
+import com.squareup.picasso.Picasso;
+import com.squareup.picasso.Target;
+
+import org.byters.bcshoppinglist.R;
+import org.byters.bcshoppinglist.controllers.ControllerMarketList;
+import org.byters.bcshoppinglist.controllers.ControllerProductList;
+import org.byters.bcshoppinglist.ui.view.ImageViewer;
+
+import java.util.ArrayList;
+
+public class ActivityMarketMap extends ActivityBase
+        implements Target {
 
     public static void display(Context context) {
         context.startActivity(new Intent(context, ActivityMarketMap.class));
     }
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_market_map);
+
+        initMap();
+    }
+
+    private void initMap() {
+
+        String uri = ControllerMarketList.getInstance().getSelectedMarketMapUri();
+        if (TextUtils.isEmpty(uri))
+            return;
+
+        Picasso.with(this).load(uri).into(this);
+    }
+
+    @Override
+    public void onBitmapLoaded(Bitmap bitmap, Picasso.LoadedFrom from) {
+        ImageViewer imageViewer = (ImageViewer) findViewById(R.id.ivMap);
+        imageViewer.loadImage(bitmap);
+
+        ArrayList<ArrayList<Point>> points = ControllerProductList.getInstance().getSelectedItemPolygon();
+        if (points == null) return;
+        for (ArrayList<Point> item : points)
+            imageViewer.addPolygon(item);
+    }
+
+    @Override
+    public void onBitmapFailed(Drawable errorDrawable) {
+
+    }
+
+    @Override
+    public void onPrepareLoad(Drawable placeHolderDrawable) {
+
+    }
 }
