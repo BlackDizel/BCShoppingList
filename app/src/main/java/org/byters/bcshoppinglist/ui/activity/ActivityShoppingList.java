118a068431bfc873ff50145b69d66bbb10d3b4bd
alexandr motorin <blackdizel@gmail.com>
2016-05-24 19:30:00 +0300

    implement shopping list toolbar

--- app/src/main/java/org/byters/bcshoppinglist/ui/activity/ActivityShoppingList.java
+++ app/src/main/java/org/byters/bcshoppinglist/ui/activity/ActivityShoppingList.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
 
 import org.byters.bcshoppinglist.R;
@@ -55,6 +56,15 @@ public class ActivityShoppingList extends ActivityBase
     private void initToolbar() {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home)
+            onBackPressed();
+
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
