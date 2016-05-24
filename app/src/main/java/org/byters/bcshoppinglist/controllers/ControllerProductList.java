2d0e75dde2b65f042fd1fc264e01c8a874522583
alexandr motorin <blackdizel@gmail.com>
2016-05-24 19:24:07 +0300

    implement search in products controller

--- app/src/main/java/org/byters/bcshoppinglist/controllers/ControllerProductList.java
+++ app/src/main/java/org/byters/bcshoppinglist/controllers/ControllerProductList.java
@@ -1,9 +1,12 @@
 package org.byters.bcshoppinglist.controllers;
 
 import android.support.annotation.Nullable;
+import android.text.TextUtils;
 
 import org.byters.bcshoppinglist.api.ServiceShoppingList;
 import org.byters.bcshoppinglist.controllers.utils.OnProductListUpdateListener;
+import org.byters.bcshoppinglist.model.Product;
+import org.byters.bcshoppinglist.model.ProductFiltered;
 import org.byters.bcshoppinglist.model.StoreCategory;
 
 import java.util.ArrayList;
@@ -17,7 +20,9 @@ public class ControllerProductList
 
     private static ControllerProductList instance;
     private ArrayList<StoreCategory> data;
+    private ArrayList<ProductFiltered> dataFiltered;
     private ArrayList<OnProductListUpdateListener> listeners;
+    private String search;
 
     public static ControllerProductList getInstance() {
         if (instance == null) instance = new ControllerProductList();
@@ -35,7 +40,6 @@ public class ControllerProductList
         listeners.remove(listener);
     }
 
-
     public void updateData() {
         ServiceShoppingList.getApi().getMarketCategoryList(ControllerMarketList.getInstance().getSelectedMarketId()).enqueue(this);
     }
@@ -44,6 +48,10 @@ public class ControllerProductList
     public void onResponse(Call<ArrayList<StoreCategory>> call, Response<ArrayList<StoreCategory>> response) {
         if (response.body() != null)
             data = response.body();
+        notifyListeners();
+    }
+
+    private void notifyListeners() {
         if (listeners != null)
             for (OnProductListUpdateListener listener : listeners)
                 listener.onProductListUpdate();
@@ -51,11 +59,8 @@ public class ControllerProductList
 
     @Override
     public void onFailure(Call<ArrayList<StoreCategory>> call, Throwable t) {
-        if (listeners != null)
-            for (OnProductListUpdateListener listener : listeners)
-                listener.onProductListUpdate();
+        notifyListeners();
     }
-
 
     @Nullable
     private StoreCategory getItem(int position) {
@@ -64,7 +69,69 @@ public class ControllerProductList
         return data.get(position);
     }
 
-    public int getItemsCount() {
-        return data == null ? 0 : data.size();
+    public int getFilteredItemsCount() {
+        if (data == null || TextUtils.isEmpty(search)) return 0;
+
+        ArrayList<ProductFiltered> dataFiltered = getDataFiltered();
+        return dataFiltered.size();
+    }
+
+    private ArrayList<ProductFiltered> getDataFiltered() {
+        if (dataFiltered != null) return dataFiltered;
+
+        dataFiltered = new ArrayList<>();
+        for (StoreCategory category : data) {
+
+            if (category.products == null) continue;
+
+            for (Product product : category.products) {
+                if (product.name.contains(search)) {
+                    ProductFiltered item = new ProductFiltered();
+                    item.categoryId = category.id;
+                    item.product = product;
+                    dataFiltered.add(item);
+                }
+            }
+        }
+        return dataFiltered;
+    }
+
+    public void search(String query) {
+        this.search = query;
+        dataFiltered = null;
+        notifyListeners();
+    }
+
+    public void clearSearch() {
+        this.search = null;
+        dataFiltered = null;
+        notifyListeners();
+    }
+
+    @Nullable
+    public String getFilteredItemTitle(int position) {
+        if (dataFiltered == null) return null;
+        ProductFiltered item = getFilteredItem(position);
+        if (item == null) return null;
+        return item.product.name;
+    }
+
+    @Nullable
+    private ProductFiltered getFilteredItem(int position) {
+        if (dataFiltered == null || position < 0 || dataFiltered.size() <= position) return null;
+        return dataFiltered.get(position);
+    }
+
+    @Nullable
+    public String getFilteredItemCategoryTitle(int position) {
+        if (dataFiltered == null || data == null) return null;
+        ProductFiltered item = getFilteredItem(position);
+        if (item == null) return null;
+
+        for (StoreCategory category : data)
+            if (category.id == item.categoryId)
+                return category.name;
+
+        return null;
     }
 }
---
+++ app/src/main/java/org/byters/bcshoppinglist/model/ProductFiltered.java
@@ -0,0 +1,6 @@
+package org.byters.bcshoppinglist.model;
+
+public class ProductFiltered {
+    public Product product;
+    public int categoryId;
+}
