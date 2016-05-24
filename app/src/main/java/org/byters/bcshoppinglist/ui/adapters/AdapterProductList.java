85d748533036d34effd4019ee8e2fabc162f5429
alexandr motorin <blackdizel@gmail.com>
2016-05-24 17:05:57 +0300

    add product list adapter stub

---
+++ app/src/main/java/org/byters/bcshoppinglist/ui/adapters/AdapterProductList.java
@@ -0,0 +1,49 @@
+package org.byters.bcshoppinglist.ui.adapters;
+
+
+import android.support.annotation.LayoutRes;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import org.byters.bcshoppinglist.R;
+
+
+public class AdapterProductList extends RecyclerView.Adapter<AdapterProductList.ViewHolder> {
+    public AdapterProductList() {
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return new ViewHolder(LayoutInflater.from(parent.getContext()).inflate(getId(), parent, false));
+    }
+
+    @LayoutRes
+    private int getId() {
+        return R.layout.view_product_list_item;
+    }
+
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        holder.setData(position);
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        public ViewHolder(View itemView) {
+            super(itemView);
+        }
+
+        public void setData(int position) {
+
+        }
+
+    }
+}
