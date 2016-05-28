6ddab4e4c14288cd42370f159d481952ee03c686
alexandr motorin <blackdizel@gmail.com>
2016-05-28 19:41:26 +0300

    fix touches and coords

--- app/src/main/java/org/byters/bcshoppinglist/ui/view/ImageViewer.java
+++ app/src/main/java/org/byters/bcshoppinglist/ui/view/ImageViewer.java
@@ -11,14 +11,10 @@ import android.graphics.Point;
 import android.graphics.Rect;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
-import android.util.DisplayMetrics;
 import android.view.GestureDetector;
-import android.view.GestureDetector.SimpleOnGestureListener;
 import android.view.MotionEvent;
 import android.view.ScaleGestureDetector;
-import android.view.ScaleGestureDetector.OnScaleGestureListener;
 import android.view.View;
-import android.widget.Scroller;
 
 import java.util.ArrayList;
 
@@ -26,53 +22,18 @@ import java.util.ArrayList;
 public class ImageViewer extends View {
     private GestureDetector gestureDetector;
     private ScaleGestureDetector scaleGestureDetector;
-    private Scroller scroller;
+    private float scaleFactor;
 
     private Paint paint;
     @Nullable
     private Bitmap image;
 
-    private float scaleFactor;
-
-    private Rect dst;
+    private Rect dst, dstOrig;
+    private int imageWidth, imageHeight;
 
     public ImageViewer(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs, 0);
         init(context);
-    }
-
-    public ImageViewer(Context context) {
-        super(context);
-
-        init(context);
-    }
-
-    private void init(Context context) {
-        gestureDetector = new GestureDetector(context, new MyGestureListener());
-        scaleGestureDetector = new ScaleGestureDetector(context, new MyScaleGestureListener());
-
-        setHorizontalScrollBarEnabled(true);
-        setVerticalScrollBarEnabled(true);
-
-        scroller = new Scroller(context);
-
-        paint = new Paint();
-        paint.setFilterBitmap(true);
-        paint.setDither(false);
-
-        image = null;
-        scaleFactor = 1;
-
-        dst = new Rect(0, 0, 0, 0);
-
-    }
-
-    private int getScaledWidth() {
-        return image == null ? 0 : (int) (image.getWidth() * scaleFactor);
-    }
-
-    private int getScaledHeight() {
-        return image == null ? 0 : (int) (image.getHeight() * scaleFactor);
     }
 
     /*    public void setPoint(Point point) {
@@ -87,6 +48,49 @@ public class ImageViewer extends View {
             invalidate();
 
         }*/
+
+    public ImageViewer(Context context) {
+        super(context);
+
+        init(context);
+    }
+
+    private void init(Context context) {
+        MyGestureListener listener = new MyGestureListener();
+        gestureDetector = new GestureDetector(context, listener);
+        scaleGestureDetector = new ScaleGestureDetector(context, listener);
+
+        paint = new Paint();
+        paint.setFilterBitmap(true);
+        paint.setDither(false);
+
+        image = null;
+
+        dst = new Rect(0, 0, 0, 0);
+        dstOrig = new Rect(0, 0, 0, 0);
+
+        scaleFactor = 1;
+    }
+
+    public void loadImage(Bitmap source) {
+        image = source.copy(source.getConfig(), true);
+        if (image == null) return;
+
+        imageHeight = image.getHeight();
+        imageWidth = image.getWidth();
+        initRects();
+
+        invalidate();
+    }
+
+    private void initRects() {
+        if (imageWidth == 0 || imageHeight == 0
+                || dstOrig.bottom == 0 || dstOrig.right == 0) return;
+        int calcHeight = imageHeight / (imageWidth / dstOrig.right);
+        dst.bottom = calcHeight;
+        dstOrig.bottom = calcHeight;
+    }
+
     public void addPolygon(ArrayList<Point> points) {
         if (image == null || points == null || points.size() < 2) return;
 
@@ -106,159 +110,81 @@ public class ImageViewer extends View {
         brush.setDither(true);
 
         can.drawPath(path, brush);
+
         invalidate();
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+
+        int width = MeasureSpec.getSize(widthMeasureSpec);
+        int height = MeasureSpec.getSize(heightMeasureSpec);
+
+        dst.set(0, 0, width, height);
+        dstOrig.set(0, 0, width, height);
+        scaleFactor = 1;
+        initRects();
     }
 
     @Override
     public void onDraw(Canvas canvas) {
         if (image == null) return;
-        //todo: set scale and translate based on matrix
-        dst.set(-getScaledWidth() / 2, -getScaledHeight() / 2, getScaledWidth() / 2, getScaledHeight() / 2);
         canvas.drawBitmap(image, null, dst, paint);
-    }
-
-    @Override
-    protected int computeHorizontalScrollRange() {
-        return getScaledWidth();
-    }
-
-    @Override
-    protected int computeVerticalScrollRange() {
-        return getScaledHeight();
     }
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
         if (image == null) return false;
 
-        // check for tap and cancel fling
-        if ((event.getAction() & MotionEvent.ACTION_MASK) == MotionEvent.ACTION_DOWN) {
-            if (!scroller.isFinished()) scroller.abortAnimation();
-        }
-        // handle pinch zoom gesture
-        // don't check return value since it is always true
         scaleGestureDetector.onTouchEvent(event);
-
-        // check for scroll gesture
-
-
         if (gestureDetector.onTouchEvent(event)) return true;
-
-        // check for pointer release
-        if ((event.getPointerCount() == 1) && ((event.getAction() & MotionEvent.ACTION_MASK) == MotionEvent.ACTION_UP)) {
-            int newScrollX = getScrollX();
-            if (getScrollX() < 0) newScrollX = 0;
-            else if (getScrollX() > image.getWidth() - getWidth())
-                newScrollX = image.getWidth() - getWidth();
-
-            int newScrollY = getScrollY();
-            if (getScrollY() < 0) newScrollY = 0;
-            else if (getScrollY() > image.getHeight() - getHeight())
-                newScrollY = image.getHeight() - getHeight();
-
-            if ((newScrollX != getScrollX()) || (newScrollY != getScrollY())) {
-                scroller.startScroll(getScrollX(), getScrollY(), newScrollX - getScrollX(), newScrollY - getScrollY());
-                awakenScrollBars(scroller.getDuration());
-            }
-        }
-
         return true;
     }
 
-    @Override
-    public void computeScroll() {
-        if (scroller.computeScrollOffset()) {
-            int oldX = getScrollX();
-            int oldY = getScrollY();
-            int x = scroller.getCurrX();
-            int y = scroller.getCurrY();
-            scrollTo(x, y);
-            if (oldX != getScrollX() || oldY != getScrollY()) {
-                onScrollChanged(getScrollX(), getScrollY(), oldX, oldY);
-            }
-
-            postInvalidate();
-        }
-    }
-
-    @Override
-    protected void onScrollChanged(int x, int y, int oldX, int oldY) {
-    }
-
-    @Override
-    protected void onSizeChanged(int width, int height, int oldWidth, int oldHeight) {
-        int scrollX = (getScaledWidth() < width ? -(width - getScaledWidth()) / 2 : getScaledWidth() / 2);
-        int scrollY = (getScaledHeight() < height ? -(height - getScaledHeight()) / 2 : getScaledHeight() / 2);
-        scrollTo(scrollX, scrollY);
-    }
-
-    public void loadImage(Bitmap source) {
-        image = source.copy(source.getConfig(), true);
-
-        if (image == null)
-            return;
-
-        DisplayMetrics displaymetrics = getResources().getDisplayMetrics();
-
-        if ((float) displaymetrics.heightPixels / (float) image.getHeight() > (float) displaymetrics.widthPixels / (float) image.getWidth())
-            scaleFactor = (float) displaymetrics.widthPixels / (float) image.getWidth();
-        else
-            scaleFactor = (float) displaymetrics.heightPixels / (float) image.getHeight();
-
-        invalidate();
-    }
-
-    private class MyGestureListener extends SimpleOnGestureListener {
+    private class MyGestureListener extends GestureDetector.SimpleOnGestureListener
+            implements ScaleGestureDetector.OnScaleGestureListener {
         @Override
         public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
-
-            scrollBy((int) distanceX, (int) distanceY);
+            int x = (int) distanceX;
+            int y = (int) distanceY;
+            x = x < 0 ? Math.max(x, -getScrollX()) : Math.min(x, dst.right - (getScrollX() + dstOrig.right));
+            y = y < 0 ? Math.max(y, -getScrollY()) : Math.min(y, dst.bottom - (getScrollY() + dstOrig.bottom));
+            scrollBy(x, y);
             return true;
         }
 
         @Override
-        public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
-            int fixedScrollX = 0, fixedScrollY = 0;
-            int maxScrollX = getScaledWidth(), maxScrollY = getScaledHeight();
-
-            if (getScaledWidth() < getWidth()) {
-                fixedScrollX = -(getWidth() - getScaledWidth()) / 2;
-                maxScrollX = fixedScrollX + getScaledWidth();
-            }
-
-            if (getScaledHeight() < getHeight()) {
-                fixedScrollY = -(getHeight() - getScaledHeight()) / 2;
-                maxScrollY = fixedScrollY + getScaledHeight();
-            }
-
-            boolean scrollBeyondImage = (fixedScrollX < 0) || (fixedScrollX > maxScrollX) || (fixedScrollY < 0) || (fixedScrollY > maxScrollY);
-            if (scrollBeyondImage) return false;
-
-            scroller.fling(getScrollX(), getScrollY(), -(int) velocityX, -(int) velocityY, 0, getScaledWidth() - getWidth(), 0, getScaledHeight() - getHeight());
-            awakenScrollBars(scroller.getDuration());
-
-            return true;
-        }
-    }
-
-    private class MyScaleGestureListener implements OnScaleGestureListener {
         public boolean onScale(ScaleGestureDetector detector) {
-            scaleFactor *= detector.getScaleFactor();
 
-            int newScrollX = (int) ((getScrollX() + detector.getFocusX()) * detector.getScaleFactor() - detector.getFocusX());
+            scaleFactor *= detector.getScaleFactor();
+            scaleFactor = Math.max(scaleFactor, 1);
+
+            int right = (int) (dstOrig.right * scaleFactor);
+            int bottom = (int) (dstOrig.bottom * scaleFactor);
+
+            dst.left = (int) Math.max(dstOrig.left * scaleFactor, dstOrig.left);
+            dst.top = (int) Math.max(dstOrig.top * scaleFactor, dstOrig.top);
+            dst.right = dst.right == 0 ? right : Math.max(right, dstOrig.right);
+            dst.bottom = dst.bottom == 0 ? bottom : Math.max(bottom, dstOrig.bottom);
+
+            //todo add scroll to fingers pos
+            /*int newScrollX = (int) ((getScrollX() + detector.getFocusX()) * detector.getScaleFactor() - detector.getFocusX());
             int newScrollY = (int) ((getScrollY() + detector.getFocusY()) * detector.getScaleFactor() - detector.getFocusY());
-            scrollTo(newScrollX, newScrollY);
+            scrollTo(newScrollX, newScrollY);*/
 
             invalidate();
-
             return true;
         }
 
+        @Override
         public boolean onScaleBegin(ScaleGestureDetector detector) {
             return true;
         }
 
+        @Override
         public void onScaleEnd(ScaleGestureDetector detector) {
+
         }
     }
 }
