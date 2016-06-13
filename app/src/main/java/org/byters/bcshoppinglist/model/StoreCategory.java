41619f758ff029085fc2e2225b6c4f27ed6e65c8
alexandr motorin <blackdizel@gmail.com>
2016-06-13 21:37:58 +0300

    add methods contains any product from parameters, add method return polygons

--- app/src/main/java/org/byters/bcshoppinglist/model/StoreCategory.java
+++ app/src/main/java/org/byters/bcshoppinglist/model/StoreCategory.java
@@ -1,5 +1,8 @@
 package org.byters.bcshoppinglist.model;
 
+import android.graphics.Point;
+import android.support.annotation.Nullable;
+
 import java.util.ArrayList;
 
 public class StoreCategory {
@@ -8,4 +11,35 @@ public class StoreCategory {
     public int id;
     public String name;
 
+    public boolean containsAny(ArrayList<ShoppingListItem> items) {
+        if (items == null || products == null) return false;
+        for (Product product : products)
+            for (ShoppingListItem item : items)
+                if (product.name.contains(item.title))
+                    return true;
+        return false;
+    }
+
+    @Nullable
+    public ArrayList<ArrayList<Point>> getPolygon() {
+        if (coords == null) return null;
+
+        ArrayList<ArrayList<Point>> result = null;
+        for (ArrayList<Coord> array : coords) {
+
+            ArrayList<Point> points = null;
+
+            for (Coord coord : array) {
+                Point point = new Point(coord.x, coord.y);
+                if (points == null) points = new ArrayList<>();
+                points.add(point);
+            }
+
+            if (points == null) continue;
+
+            if (result == null) result = new ArrayList<>();
+            result.add(points);
+        }
+        return result;
+    }
 }
